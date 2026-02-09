b4559605638d6e1f373be0bc6aa97bc8c67f1f9f
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2026-02-09 17:24:57 +0700

    cell05/ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+    org_arr = [2, 8, 9, 48, 8, 22,-12, 2]
+    new_arr = []
+    for i in org_arr:
+        new_arr.append(i+2)
+    print(f"Original array: {org_arr}\nNew array: {new_arr}")
+
+main()
